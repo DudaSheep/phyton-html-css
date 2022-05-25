f0bb82e63a18c8d3a45da378f08d874e47317920
DudaSheep <meduarda.carneiro@hotmail.com>
2022-05-25 00:30:40 +0000

    aula2

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
-app = Flask("Hello")  #cria um app
+DATABASE = "banco.bd"     #caminho pro bancodedadosBD
+SECRET_KEY = "chavedobd"  #senha secreta do BD
 
-@app.route("/")  #rota raiz
-@app.route("/hello")  #regisra uma rota e chama uma funçao def
-def hello():
-    return render_template("hello.html")
+app = Flask("Hello")      #cria um app
+app.config.from_object(__name__)
+
+def conecta_bd():
+    return sqlite3.connect(DATABASE)
+
+#ABRE/FECHA A CONECÇAO  COM O BANCO
+@app.before_request    
+def antes_requisicao():
+    g.bd = conecta_bd()
+
+@app.teardown_request
+def depois_requesiçao(e):
+    g.bd.close() 
+
+
+@app.route("/")       #rota raiz
+def exibir_entradas():    
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, texto, criado_em in cur.fetchall():
+        entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
+    return render_template("layout.html", entradas=entradas)
+
+
+
+#@app.route("/hello")  #regisra uma rota e chama uma funçao def
+#def hello():
+#    return render_template("hello.html")
+
+
+
 #def hello():
 #    return "Hello Claire!!"
 
